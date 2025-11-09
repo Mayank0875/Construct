37d322dedd8c8437b88d8f76217a052e3a4ac022
Mayank0875 <mayankgupta0875@gmail.com>
2025-11-09 18:01:29 +0530

    Initial setup

---
+++ AutoGraderSystem/main.py
@@ -0,0 +1,13 @@
+
+'''construct hackathon we are comming to win you'''
+
+
+print('''Welcome to the AutoGrader System!
+      This system is designed to help educators efficiently grade student assignments.
+      Imprtant thing to remember:
+        1. Make sure to make different branches for different features.
+        2. Commit your changes often.
+        3. Write clear and concise commit messages.
+        4. don't push directly to the main branch.
+        Happy Coding!
+''')
